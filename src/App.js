3bea12789b405b18e1ebba44e08edd7fc6be2281
TimoteoGambella <91702810+TimoteoGambella@users.noreply.github.com>
2023-01-09 11:17:12 -0300

    Update App.js
    posible app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ApiContext } from "./context/ApiContext.jsx";
+import "./styles/styles.scss";
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mobile-div">
+      <ApiContext>
+        <Router>
+          <Routes>
+            <Route path="/" element={<></>} />
+          </Routes>
+        </Router>
+      </ApiContext>
     </div>
   );
 }
