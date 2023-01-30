6391d6f19824bfe9836ff9dfb995efcab782dea1
Gheraldine Salazar <103468156+GheraldineSalazarL@users.noreply.github.com>
2023-01-30 00:22:38 -0300

    App Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import Login from "./components/Login/Login.jsx";
-import Navbar from "./components/Navbar/Navbar.jsx";
-import Sigin from "./components/Sigin/Sigin.jsx";
+
 import { ApiContext } from "./context/ApiContext.jsx";
 import "./styles/styles.scss";
-import Home from "./views/Home.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import ProjectListContainer from "./components/Projects/ProjectListContainer/ProjectListContainer.jsx";
-import Proyectos from "./views/Proyectos.jsx";
-import NavbarProjects from "./components/NavbarProjects/NavbarProjects.jsx";
-import ProjectDetailContainer from "./components/Projects/ProjectDetailContainer/ProjectDetailContainer.js";
+
+import { LoginProvider } from "./context/LoginContext.jsx";
+import AppRouter from "./router/AppRouter.jsx";
 
 function App() {
+
+
 
   return (
     // <ApiContext>
@@ -21,26 +18,10 @@ function App() {
     //     </Routes>
     //   </Router>
     // </ApiContext>
-      <BrowserRouter>
-        <Navbar/>
-        <NavbarProjects/>
-          <Routes>
-            {/* <Route path="/login" element={<Login/>} /> */}
-            {/* <Route path="/sigin" element={<Sigin/>} /> */}
-            <Route path="*" element={<Navigate to ="/"/>} />
-            <Route path="/" element={<Home/>} />
-            <Route path="/proyectos" element={<Proyectos/>} />
-            <Route path="/proyectos/:categoryId" element={<Proyectos/>}/>
-            {/* <Route path="/project" element={<ProjectDetailContainer/>}/> */}
-            <Route path="/proyecto/:proyectoId" element={<ProjectDetailContainer/>}/>
-            
 
-            
-              
-          </Routes>
-         
-        <Footer/>
-      </BrowserRouter>
+    <LoginProvider>
+        <AppRouter/>
+    </LoginProvider>
   );
 }
 
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { LoginContext } from "../context/LoginContext";
+import { useContext } from "react";
+import Home from "../views/Home.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import Proyectos from "../views/Proyectos.jsx";
+import NavbarProjects from "../components/NavbarProjects/NavbarProjects.jsx";
+import ProjectDetailContainer from "../components/Projects/ProjectDetailContainer/ProjectDetailContainer.js";
+import Login from "../components/Login/Login.jsx";
+import Navbar from "../components/Navbar/Navbar.jsx";
+import Sigin from "../components/Sigin/Sigin.jsx";
+
+const AppRouter = () => {
+
+    const {user} = useContext(LoginContext)
+
+  return (
+    <BrowserRouter>
+           <Navbar/>
+            <NavbarProjects/>
+            <Routes>
+
+                {
+                    user.logged
+                    ? <Route path="/login" element={<Navigate to ="/"/>} />
+                    : <Route path="/login" element={<Login/>} />
+                }
+                
+                <Route path="/sigin" element={<Sigin/>} />
+                <Route path="*" element={<Navigate to ="/"/>} />
+                <Route path="/" element={<Home/>} />
+                <Route path="/proyectos" element={<Proyectos/>} />
+                <Route path="/proyectos/:categoryId" element={<Proyectos/>}/>
+                <Route path="/proyectos/:categoryId/:proyectoId" element={<ProjectDetailContainer/>}/>
+            </Routes>
+            <Footer/>
+        
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
