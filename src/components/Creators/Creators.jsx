d6c89ce6b2358da7acdec943b3f664b220fa8995
Gheraldine Salazar <103468156+GheraldineSalazarL@users.noreply.github.com>
2023-02-08 17:53:44 -0300

    Componente Creators

--- src/components/Creators/Creators.jsx
+++ src/components/Creators/Creators.jsx
@@ -1,39 +1,53 @@
 import React from 'react'
 import { useState } from 'react'
 import { SiGmail } from "react-icons/si";
-import { BsLinkedin, BsGithub, BsInstagram } from "react-icons/bs";
+import { BsLinkedin, BsGithub, BsBehance } from "react-icons/bs";
+import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const Creators = ({creador, props}) => {
 
     const [hover, setHover] = useState(false)
     const handleMouseEnter =() => {
         setHover(true)
-        console.log("te veo")
     }
 
     const handleMouseLeave = () => {
         setHover(false)
     }
 
+    useEffect(()=>{
+        const handleClose = (e) =>{
+            if(e.target.className!== "modal" && e.target.className!== "modalContent" && e.target.className!== "iconRedesModal"){
+                setHover(false)
+            }
+        }
+
+        window.addEventListener('click', handleClose)
+
+        return () => {
+            window.removeEventListener('click', handleClose)
+        }
+    })
 
 
   return (
     <>
-        <img onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} src={creador.foto} alt="" />
+        <img onMouseEnter={handleMouseEnter} src={creador.foto} alt="" />
         {
             hover 
             && 
-            <div className='modal'>
+            <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} className='modal'>
                 <div className='modalContent'>
                     <img src={creador.foto} alt="" />
                     <h5>{creador.nombre}</h5>
                     <p>{creador.cargo}</p>
                     <div className='d-flex-row'>
-                        <BsLinkedin className='iconRedesModal'/>
+                        <a href={`${creador.red1}`} target='blank' ><BsLinkedin className='iconRedesModal'/></a>
                         {
-                            creador.id===2 ? <BsInstagram className='iconRedesModal'/> : <BsGithub className='iconRedesModal'/>
+                            creador.id===2 ? <a href={`${creador.red2}`} target="blank"><BsBehance className='iconRedesModal'/></a> : <a href={`${creador.red2}`} target="blank"><BsGithub className='iconRedesModal'/></a>
                         }
-                        <SiGmail className='iconRedesModal'/>
+                        <a href={`${creador.red3}`} target="blank"><SiGmail className='iconRedesModal'/></a>
                     </div>
                 </div>
             </div>
--- src/components/Creators/CreatorsList.jsx
+++ src/components/Creators/CreatorsList.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import Creators from './Creators'
+import Creator1 from '../../assets/timoteo.png'
+import Creator2 from '../../assets/erik.png'
+import Creator3 from '../../assets/gheraldine.png'
 
 const creadores = [
     {
         id: 1,
-        foto: "https://via.placeholder.com/250",
+        foto: Creator1,
         nombre: "Timoteo Gambella",
         cargo: "Lider y Desarrollador",
-        linkedin: "",
-        instagram: "",
-        email: ""
+        red1: "https://www.linkedin.com/in/timoteo-gambella-4b6418210",
+        red2: "https://github.com/TimoteoGambella",
+        red3: "mailto:timi.gambella@hotmail.com"
     },
     {
         id: 2,
-        foto: "https://via.placeholder.com/250",
+        foto: Creator2,
         nombre: "Erick Emmanuel Magallán Gonzales",
         cargo: "Diseñador UX/UI",
-        linkedin: "",
-        instagram: "",
-        email: ""
+        red1: "https://www.linkedin.com/in/erickmagallan/",
+        red2: "https://www.behance.net/erickmagallan",
+        red3: "mailto:arqeemg@gmail.com"
     },
     {
         id: 3,
-        foto: "https://via.placeholder.com/250",
+        foto: Creator3,
         nombre: "Gheraldine Salazar Lasso",
         cargo: "Desarrolladora",
-        linkedin: "",
-        instagram: "",
-        email: ""
+        red1: "https://www.linkedin.com/in/gheraldin-salazar-lasso/",
+        red2: "https://github.com/GheraldineSalazarL",
+        red3: "mailto:gh.eeraldin@gmail.com"
     }
 ]
 
