5a185eb4cb76169e26256156456e81d75d6a310d
Gheraldine Salazar <103468156+GheraldineSalazarL@users.noreply.github.com>
2023-02-08 11:16:56 -0300

    Projects Category Firestore

--- src/components/Projects/ProjectListContainer/ProjectListContainerCategory.jsx
+++ src/components/Projects/ProjectListContainer/ProjectListContainerCategory.jsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import { pedirDatosProyectos } from '../../../Helpers/PedirDatos'
 import ProjectList from '../ProjectList/ProjectList'
 import { FaArrowRight  } from  "react-icons/fa";
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import {db} from '../../../context/ApiContext'
 
 const ProjectListContainerCategory = () => {
 
@@ -11,22 +13,31 @@ const ProjectListContainerCategory = () => {
 
     const Categories = []
     for (let i=0; i<proyectos.length; i++){
-        if(!Categories.includes(proyectos[i].category)) Categories.push(proyectos[i].category)
+        if(!Categories.includes(proyectos[i].categoria)) Categories.push(proyectos[i].categoria)
     }
 
     const categoryId = Categories[Math.floor(Math.random() * Categories.length)];
 
     useEffect(() => {
-        pedirDatosProyectos()
-            .then((res) => {
-                if (!categoryId){
-                    setProyectos(res)
-                }else if(categoryId==="destacados"){
-                    setProyectos(res.filter((proyecto) => proyecto.destacada === true))
-                }else{
-                    setProyectos(res.filter((proyecto) => proyecto.category === categoryId))
-                }
+        // pedirDatosProyectos()
+        //     .then((res) => {
+        //         if (!categoryId){
+        //             setProyectos(res)
+        //         }else if(categoryId==="destacados"){
+        //             setProyectos(res.filter((proyecto) => proyecto.destacada === true))
+        //         }else{
+        //             setProyectos(res.filter((proyecto) => proyecto.category === categoryId))
+        //         }
                 
+        //     })
+        //     .catch((error)=>console.log(error))
+
+        const proyectosRef = collection(db, 'proyectos')
+        const q = categoryId ? query(proyectosRef, where('categoria', '==', categoryId)) : proyectosRef
+        getDocs(q)
+            .then((resp) =>{
+                const proyectosDB = resp.docs.map((doc) => ({id:doc.id, ...doc.data()}))
+                setProyectos(proyectosDB)
             })
             .catch((error)=>console.log(error))
     }, [categoryId])
@@ -41,7 +52,7 @@ const ProjectListContainerCategory = () => {
     <div>
         <div className='Headers'>
             <div className='SubTitle d-flex-row font-w-400'>
-            <h3>Proyectos de {categoryId ? categoryId.charAt(0).toUpperCase() + categoryId.slice(1) : ""}</h3>
+            <h3>Proyectos de {categoryId ? categoryId : ""}</h3>
             <button onClick={handleAfter} className='ArrowRight d-flex-row font-roboto-cond'>
                 <p>Siguiente proyecto</p>
                 <FaArrowRight className='icon'/>
